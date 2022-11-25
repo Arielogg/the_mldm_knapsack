a8deef501f4727d2a1924bf781ad137cfcda88f5
Thomas <91718856+mt06835u@users.noreply.github.com>
2022-11-25 10:41:32 +0100

    Update middle.py (with comments)
    Adding comments to the code

--- middle.py
+++ middle.py
@@ -7,16 +7,17 @@ def comb(a) :
     for i in combinaison :
         sumvalues.append(sum(i))
 
-    #retourne une liste de toutes les combinaisons possibles
-    #et une liste de la somme de chaque combinaison
+    # Return a list of all the posible combination
+    # and a list of the sum of the values for each combination
     return combinaison,sumvalues
 
 def Middle(Lv, Lw, W, optimal) :
     #On divise notre ensemble d'éléments en deux ensembles d'éléments A et B
+    # We devide our objects in 2 sets named A and B
     ALw,ALv = [Lw[:2], Lv[:2]]
     BLw,BLv = [Lw[2:], Lv[2:]]
     
-    #calcule des poids et des valeurs de tous les sous-ensembles de chaque ensemble
+    # Calculation of the weights and the values of all the subset of each set
     Acomweight,Asumweight = comb(ALw)
     Acombvalues,Asumvalues = comb(ALv)
 
@@ -25,6 +26,8 @@ def Middle(Lv, Lw, W, optimal) :
   
     #On cherche à trouver le sous-ensemble de B de plus grande valeur 
     #de sorte que le poids combiné est inférieur à W
+    # We are looking for the best subset of B in terms of value such as the combination 
+    # the assossiated weights is inferior to the capacity of the bag
     max = 0
     SacV = []
     SacD = []
@@ -44,7 +47,7 @@ capacity_path = 'low-dimensional-optimum/'+'f1_l-d_kp_10_269'
 values, weights, capacity = read_knapsack(items_path)
 optimal = read_optimal(capacity_path)
 
-# Calling the value greedy
+# Calling the greedy value
 values = list(map(int, values))
 weights = list(map(int, weights))
 
